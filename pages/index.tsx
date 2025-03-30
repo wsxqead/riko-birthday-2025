96786edd30235c895f1c0c599fad7f7c7acd0fee
zsx528 <zsx528@gmail.com>
2025-03-30 17:17:22 +0900

    feat: 메인 이미지 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { gifts } from "@/data/gifts";
 import "../styles/globals.css"; // ✅ 스타일 적용
 
@@ -6,6 +7,18 @@ export default function Home() {
   return (
     <div className="container">
       <h1>🎉 유즈하 리코 생일 선물 🎉</h1>
+
+      {/* 🎀 생일 이미지 삽입 */}
+      <div className="main-image">
+        <Image
+          src="/images/happyday.png"
+          alt="리코 생일 축하 이미지"
+          width={640}
+          height={300}
+          layout="intrinsic"
+        />
+      </div>
+
       <ul>
         {gifts.map((gift) => (
           <li key={gift.id}>
